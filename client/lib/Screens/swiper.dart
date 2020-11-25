7bf7cc154aa1f5c6e315f4a7ae31ec6ef1cabb1d
Alex Dang <alexdang1993374@gmail.com>
2020-11-25 15:32:01 +0700

    can get images

--- client/lib/Screens/swiper.dart
+++ client/lib/Screens/swiper.dart
@@ -1,23 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tindercard/flutter_tindercard.dart';
-
-
-class App extends StatefulWidget {
-  _AppState createState() => _AppState();
-}
-
+import 'package:dio/dio.dart';
 
 class Swiper extends StatefulWidget {
   static String routeName = "/swiper";
-   _AppState createState() => _AppState();
+  _AppState createState() => _AppState();
 }
 
-class _AppState extends State<App> {
-
- @override
-
+class _AppState extends State<Swiper> {
+  @override
   Widget build(BuildContext context) {
-  
     return MaterialApp(
       title: "Movie Night",
       debugShowCheckedModeBanner: false,
@@ -36,7 +28,13 @@ class Tinderswiper extends StatefulWidget {
 
 class _TinderswiperState extends State<Tinderswiper>
     with TickerProviderStateMixin {
+  List<String> movies = [];
+
   List<String> movieImages = [
+    "https://occ-0-2851-38.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABbfzWC5RVgFT8kQt7PgHVZC8NxDbqPHwOc-ItTMpw9As4MPReNZF6WXi9OON2_2eekS6xshj8wsCEzVcvattQT5vsEuI9t-T_wExyo6FsA90ibRTkP8MQ3Oy9QA.jpg?r=848",
+    "https://occ-0-2851-38.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABebXub2bi247otQst_hOZO3nGA_tmisdBih1FgmhhDXizhqyNhSJKmcCkW9EjZSuLw9-4b-yvc89Lf2zK2Zh4Ab4d3dciKVOOTfjYUFoNy96k7C63bzHlETPUaM.jpg?r=08c",
+    "https://occ-0-2851-38.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABZsccQeCfH1sB-4d1WnIKXXEbqLHYALSo1QCOrGrd5vuwXjLSSiNuIfxYmscIU7vRavU6929JPe1NILHC5vZYnKfUMsnhr4IFmnUefvFCgNO3i-vkid-lwt5GXU.jpg?r=4fa",
+    "https://occ-0-2717-360.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABbfqo7mBju7ZQXKtGagr_sgrd13DUqzf8-p6OZeoQbop0fToj4K9JImmM2DvNh2HmPt6_NW6Xnb4NLYUxuWH9ttAEGOPbX7mc0cPLpkX_Nrp18qZVyrr7ZpHVvc.jpg?r=f7e",
     "assets/img/bored.jpg",
     "assets/img/carumba.jpg",
     "assets/img/chocolate.jpg",
@@ -47,8 +45,24 @@ class _TinderswiperState extends State<Tinderswiper>
     "assets/img/firm.jpg",
     "assets/img/god.jpg",
   ];
+
+  void getHttp() async {
+    try {
+      Response response = await Dio().get(
+          "https://asia-northeast1-movie-night-cc.cloudfunctions.net/getAllMovies");
+      var pics = response.data;
+      for (var i = 0; i < pics.length; i++) {
+        movieImages.add(pics[i]["img"]);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getHttp();
+    print(movieImages);
     return Scaffold(
       appBar: AppBar(
         title: Text("Swipe Movies"),
@@ -58,7 +72,7 @@ class _TinderswiperState extends State<Tinderswiper>
           height: MediaQuery.of(context).size.height * 0.6,
           child: TinderSwapCard(
             orientation: AmassOrientation.TOP,
-            totalNum: 9,
+            totalNum: 20,
             stackNum: 3,
             maxWidth: MediaQuery.of(context).size.width * 0.9,
             maxHeight: MediaQuery.of(context).size.width * 0.9,
@@ -67,8 +81,8 @@ class _TinderswiperState extends State<Tinderswiper>
             cardBuilder: (context, index) => Card(
               child: Padding(
                   padding: EdgeInsets.all(20.0),
-                  child: Image.asset(
-                    '${movieImages[index]}',
+                  child: Image.network(
+                    movieImages[index],
                     fit: BoxFit.fill,
                   )),
               elevation: 10.0,
--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:flutter_tindercard/flutter_tindercard.dart';
-import 'package:dio/dio.dart';
-=======
 import 'package:firebase_core/firebase_core.dart';
 import 'Screens/swiper.dart';
 import 'routes.dart';
->>>>>>> 40efe113fe1db931d0b1adcc7efcc6068d53e562
 
 void main() {
   runApp(App());
@@ -46,14 +41,14 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     // Show error message if initialization failed
-    if (_error) {
-      return Text("Something went wrong");
-    }
+    // if (_error) {
+    //   return Text("Something went wrong");
+    // }
 
-    // Show a loader until FlutterFire is initialized
-    if (!_initialized) {
-      return Text("Loading");
-    }
+    // // Show a loader until FlutterFire is initialized
+    // if (!_initialized) {
+    //   return Text("Loading");
+    // }
 
     return MaterialApp(
       title: "Movie Night",
@@ -61,75 +56,8 @@ class _AppState extends State<App> {
       theme: ThemeData(
           primaryColor: Colors.white,
           scaffoldBackgroundColor: Colors.grey[100]),
-<<<<<<< HEAD
-      home: Tinderswiper(),
-    );
-  }
-}
-
-class Tinderswiper extends StatefulWidget {
-  @override
-  _TinderswiperState createState() => _TinderswiperState();
-}
-
-class _TinderswiperState extends State<Tinderswiper>
-    with TickerProviderStateMixin {
-  void getHttp() async {
-    try {
-      Response response = await Dio().get(
-          "https://asia-northeast1-movie-night-cc.cloudfunctions.net/getAllUsers");
-      print(response);
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  List<String> movieImages = [
-    "assets/img/bored.jpg",
-    "assets/img/carumba.jpg",
-    "assets/img/chocolate.jpg",
-    "assets/img/wumbo.jpg",
-    "assets/img/dead.jpg",
-    "assets/img/dish.jpg",
-    "assets/img/fat.jpg",
-    "assets/img/firm.jpg",
-    "assets/img/god.jpg",
-  ];
-  @override
-  Widget build(BuildContext context) {
-    getHttp();
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Swipe Movies"),
-      ),
-      body: Center(
-        child: Container(
-          height: MediaQuery.of(context).size.height * 0.6,
-          child: TinderSwapCard(
-            orientation: AmassOrientation.TOP,
-            totalNum: 9,
-            stackNum: 3,
-            maxWidth: MediaQuery.of(context).size.width * 0.9,
-            maxHeight: MediaQuery.of(context).size.width * 0.9,
-            minWidth: MediaQuery.of(context).size.width * 0.8,
-            minHeight: MediaQuery.of(context).size.width * 0.8,
-            cardBuilder: (context, index) => Card(
-              child: Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Image.asset(
-                    '${movieImages[index]}',
-                    fit: BoxFit.fill,
-                  )),
-              elevation: 10.0,
-            ),
-          ),
-        ),
-      ),
-=======
       home: Swiper(),
       routes: routes,
-      
->>>>>>> 40efe113fe1db931d0b1adcc7efcc6068d53e562
     );
   }
 }
