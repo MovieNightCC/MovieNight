89537d7ce6fed6eda28908202b0fdb653e51bf20
Jack Fowler <OnigiriJack@github.com>
2020-11-25 13:54:45 +0900

    adding flutter fire configuration files

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tindercard/flutter_tindercard.dart';
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,8 +11,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  // Set default `_initialized` and `_error` state to false
+  bool _initialized = false;
+  bool _error = false;
+
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    void initState() {
+      initializeFlutterFire();
+      super.initState();
+    }
+
     return MaterialApp(
       title: "Movie Night",
       debugShowCheckedModeBanner: false,
