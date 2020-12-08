34cd88db4418890cc4a457fdd9b050ec6f62c240
Jose M. Torres <jmtorresr@gmail.com>
2020-12-08 15:39:31 +0900

    New APK for Production

--- client/ios/Flutter/flutter_export_environment.sh
+++ client/ios/Flutter/flutter_export_environment.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/mrcheezits/dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/mrcheezits/dev/stageApp/MovieNight/client"
+export "FLUTTER_ROOT=/Users/3301hakuba/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/3301hakuba/Dropbox/CC15/MovieNight/client"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-<<<<<<< HEAD
 export "FLUTTER_FRAMEWORK_DIR=/Users/3301hakuba/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=1.3.0"
-export "FLUTTER_BUILD_NUMBER=3"
-=======
-export "FLUTTER_FRAMEWORK_DIR=/Users/mrcheezits/dev/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
->>>>>>> 483e2e0cad6647d26711b0de87986191cdf1412e
+export "FLUTTER_BUILD_NAME=1.4.0"
+export "FLUTTER_BUILD_NUMBER=4"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
