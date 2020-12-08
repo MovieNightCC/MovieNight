6a12b60c3a28f8ef61e9492ef233659b0748e523
Jack Fowler <OnigiriJack@github.com>
2020-12-08 14:18:08 +0900

    upadated cloud function boolean error

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -34,22 +34,7 @@ export const helloWorld = functions
     response.send("Hello from Firebase!");
   });
 
-export const createGame = functions
-  .region("asia-northeast1")
-  .https.onRequest(async (request: any, response) => {
-    const gameRef = db.collection("pairs").doc();
-    await gameRef.set({
-      name: "peter",
-      age: "19",
-      arr: [1, 2, 3, 4],
-      obj: {
-        pp: "hard",
-      },
-    });
-    const snapShot = await gameRef.get();
-    const data = snapShot.data();
-    response.send(data);
-  });
+
 
 export const userRecommendAlgo = functions.firestore
   .document("users/{docId}")
@@ -175,7 +160,7 @@ export const userRecommendAlgo = functions.firestore
               MusicInspired: oldUserData["recommendations"]["MusicInspired"] + 1,
               Romance: oldUserData["recommendations"]["Romance"] - 0.1,
               Scifi: oldUserData["recommendations"]["Scifi"] - 0.1,
-              Superhero: oldUserData["recommendations"]["Superhero"] - 0.1
+              Superhero: oldUserData["recommendations"]["Superhero"] - 0.1,
             },
           },
           { merge: true }
@@ -514,7 +499,7 @@ export const startGame = functions
     const result = await gameRef.get();
  
     gameRef.set({
-     started: false,
+     started:true,
     },{merge  : true}).then(_=>console.log("success")).catch(_=>console.error("did not set"))
 
     const data = result.data();
@@ -531,7 +516,7 @@ export const resetGame = functions
     const result = await gameRef.get();
  
     gameRef.update({
-     started: true,
+     started: false,
 
     }).then(_=>console.log("success")).catch(_=>console.error("did not set"))
     const data = result.data();
