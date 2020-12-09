02faacefcac6a218ea52f1d33822df8898a00d6b
unknown <alexdang1993374@gmail.com>
2020-12-09 09:37:16 +0700

    stable

--- client/lib/Screens/signupform.dart
+++ client/lib/Screens/signupform.dart
@@ -3,7 +3,7 @@ import 'package:provider/provider.dart';
 import '../sizeconfig.dart';
 import '../constants.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:movie_night/screens/auth.dart';
+
 import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
 import './addPairPage.dart';
