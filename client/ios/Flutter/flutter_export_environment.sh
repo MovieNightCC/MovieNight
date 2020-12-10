1822a861176f7e74fbd5df7aa504bb630c63570f
Jack Fowler <OnigiriJack@github.com>
2020-12-10 12:01:13 +0900

    resolving last conflict

--- client/ios/Flutter/flutter_export_environment.sh
+++ client/ios/Flutter/flutter_export_environment.sh
@@ -1,24 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-<<<<<<< HEAD
 export "FLUTTER_ROOT=/Users/mrcheezits/dev/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/mrcheezits/dev/stageApp/MovieNight/client"
 export "FLUTTER_TARGET=/Users/mrcheezits/dev/stageApp/MovieNight/client/lib/main.dart"
-=======
-
->>>>>>> d77f836a52041b7cb7b737e21b83be4ec3aa7113
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-<<<<<<< HEAD
 export "FLUTTER_FRAMEWORK_DIR=/Users/mrcheezits/dev/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.3.0"
 export "FLUTTER_BUILD_NUMBER=3"
-=======
-
-export "FLUTTER_BUILD_NAME=1.5.0"
-export "FLUTTER_BUILD_NUMBER=5"
->>>>>>> d77f836a52041b7cb7b737e21b83be4ec3aa7113
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
