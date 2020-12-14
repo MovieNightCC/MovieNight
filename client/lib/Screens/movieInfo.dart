d9723301f0db36ba9fe23848441815c3c9e69d52
unknown <alexdang1993374@gmail.com>
2020-12-14 13:19:41 +0700

    runtime style

--- client/lib/Screens/movieInfo.dart
+++ client/lib/Screens/movieInfo.dart
@@ -4,7 +4,7 @@ import 'package:url_launcher/url_launcher.dart';
 import 'dart:async';
 
 String printDuration(Duration duration) {
-  String twoDigits(int n) => n.toString().padLeft(2, "0");
+  String twoDigits(int n) => n.toString();
   String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
   return "${twoDigits(duration.inHours)}:$twoDigitMinutes";
 }
